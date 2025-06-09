48f1b8b0461eb8223ae63ceb2221756631d8f9d6
Yasin <98113441+yasin-git@users.noreply.github.com>
2025-06-09 17:52:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-// 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'; // React kütüphanesini içe aktar
+import ReactDOM from 'react-dom/client'; // React DOM işlemleri için istemci tarafı kütüphanesi
+import App from './App'; // Ana uygulama bileşeni
 import {
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query';
+} from '@tanstack/react-query'; // React Query kütüphanesi
 
+// Yeni bir QueryClient örneği oluştur
 const queryClient = new QueryClient();
 
+// Uygulamayı DOM'a render et
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // React Query ile veri yönetimi için QueryClientProvider ile sar
   <QueryClientProvider client={queryClient}>
-    <App />
+    <App /> {/* Ana uygulama bileşenini çağır */}
   </QueryClientProvider>
 );
